bf1daec1c67d7ceb28ccd0b298842833689a1bac
Alexander Dreweke <alexander@dreweke.net>
2013-05-19 20:46:46 +0200

    rework install process, more complex svn repo

--- setup/INSTALL.sh
+++ setup/INSTALL.sh
@@ -3,27 +3,19 @@
 # install necesarry components
 aptitude install apache2-mpm-worker libapache2-svn libapache2-mod-auth-ntlm-winbind subversion rsync
 
-# prepare repository
-mkdir -p /var/www/svn
-svnadmin create /var/www/svn/test
-svn co file:///var/www/svn/test /tmp/test
-mkdir   /tmp/test/tags /tmp/test/branches /tmp/test/trunk
-svn add /tmp/test/tags
-svn add /tmp/test/branches
-svn add /tmp/test/trunk
-svn ci  /tmp/test -m "create SVN structure"
-rm -rf  /tmp/test
-chown -R www-data:www-data /var/www/svn
-
-# rsync config
+# copy config
 rsync -acHv --no-p --no-o --no-g conf/* /
+
+# prepare repository
+/sbin/create_svn_test
 
 # prepare ntlm
 service winbind restart
-echo "Pass: svntest"
-adduser svnuser
-echo "Pass: svntest"
-smbpasswd -a svnuser
+adduser -p `openssl passwd -1 svnpass` svnuser
+smbpasswd -a svnuser <<EOF
+svnpass
+svnpass
+EOF
 
 a2enmod auth_basic auth_digest auth_ntlm_winbind dav dav_fs dav_lock dav_svn ssl
 a2ensite default default-ssl
