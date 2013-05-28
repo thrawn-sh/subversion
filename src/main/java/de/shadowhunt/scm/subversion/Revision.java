d6a96a7407cb4366511c1ff2c8c8c581310f8633
Alexander Dreweke <alexander@dreweke.net>
2013-05-28 21:37:23 +0200

    added javadoc

--- src/main/java/de/shadowhunt/scm/subversion/Revision.java
+++ src/main/java/de/shadowhunt/scm/subversion/Revision.java
@@ -2,14 +2,29 @@ package de.shadowhunt.scm.subversion;
 
 import java.io.Serializable;
 
+/**
+ * {code Revision} defines the revision of a repository or a resource in that repository
+ */
 public final class Revision implements Comparable<Revision>, Serializable {
 
+	/**
+	 * Represents the newest {@link Revision} in the repository
+	 */
 	public static final Revision HEAD = new Revision(-1);
 
+	/**
+	 * Represents the first {@link Revision} in the repository
+	 */
 	public static final Revision INITIAL = new Revision(0);
 
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Create a new {@link Revision} instance for the given value
+	 * @param revision value of the {@link Revision} must be greater than 0
+	 * @throws IllegalArgumentException if revision is smaller or equal 0
+	 * @return the new {@link Revision} instance with the given value
+	 */
 	public static Revision create(final int revision) {
 		if (revision <= 0) {
 			throw new IllegalArgumentException("revision must be greater than 0, was " + revision);
