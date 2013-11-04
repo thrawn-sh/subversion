3a9a3bc7c4781e097740d15aec27f4e4a0731c30
Alexander Dreweke <alexander@dreweke.net>
2013-11-04 14:52:51 +0100

    added v1_7 (httpv2) tests

---
+++ src/test/java/de/shadowhunt/subversion/internal/httpv2/v1_7/Helper.java
@@ -0,0 +1,105 @@
+/*
+ * #%L
+ * Shadowhunt Subversion
+ * %%
+ * Copyright (C) 2013 shadowhunt
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package de.shadowhunt.subversion.internal.httpv2.v1_7;
+
+import java.io.File;
+import java.net.URI;
+import java.util.UUID;
+
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.Credentials;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+
+import de.shadowhunt.subversion.Repository;
+import de.shadowhunt.subversion.RepositoryFactory;
+
+final class Helper {
+
+	private static final File BASE = new File("src/test/resources/v1_7");
+
+	private static final URI DUMP_URI = URI.create("http://10.155.50.104/dump.zip");
+
+	private static final String PASSWORD = "svnpass";
+
+	private static final URI REPOSITORY_URI = URI.create("http://10.155.50.104/svn-basic/test");
+
+	private static Repository repositoryA, repositoryB;
+
+	private static final File ROOT = new File(BASE, "dump");
+
+	private static final UUID TEST_ID = UUID.randomUUID();
+
+	private static final String USERNAME_A = "svnuser";
+
+	private static final String USERNAME_B = "svnuser2";
+
+	public static File getBase() {
+		return BASE;
+	}
+
+	public static URI getDumpUri() {
+		return DUMP_URI;
+	}
+
+	static Repository getRepositoryA() {
+		if (repositoryA == null) {
+			final DefaultHttpClient client = new DefaultHttpClient();
+			final HttpContext context = new BasicHttpContext();
+
+			final CredentialsProvider cp = new BasicCredentialsProvider();
+			final Credentials credentials = new UsernamePasswordCredentials(USERNAME_A, PASSWORD);
+			cp.setCredentials(AuthScope.ANY, credentials);
+			client.setCredentialsProvider(cp);
+
+			repositoryA = RepositoryFactory.createRepository(REPOSITORY_URI, client, context);
+
+		}
+		return repositoryA;
+	}
+
+	static Repository getRepositoryB() {
+		if (repositoryB == null) {
+			final DefaultHttpClient client = new DefaultHttpClient();
+			final HttpContext context = new BasicHttpContext();
+
+			final CredentialsProvider cp = new BasicCredentialsProvider();
+			final Credentials credentials = new UsernamePasswordCredentials(USERNAME_B, PASSWORD);
+			cp.setCredentials(AuthScope.ANY, credentials);
+			client.setCredentialsProvider(cp);
+
+			repositoryB = RepositoryFactory.createRepository(REPOSITORY_URI, client, context);
+
+		}
+		return repositoryB;
+	}
+
+	static File getRoot() {
+		return ROOT;
+	}
+
+	static UUID getTestId() {
+		return TEST_ID;
+	}
+}
---
+++ src/test/java/de/shadowhunt/subversion/internal/httpv2/v1_7/SuiteIT.java
@@ -0,0 +1,53 @@
+/*
+ * #%L
+ * Shadowhunt Subversion
+ * %%
+ * Copyright (C) 2013 shadowhunt
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package de.shadowhunt.subversion.internal.httpv2.v1_7;
+
+import org.junit.FixMethodOrder;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@RunWith(Suite.class)
+@SuiteClasses({ Prepare.class, // pull dump.zip
+		// read only
+		RepositoryExists.class, //
+		RepositoryDownloadUri.class, // uses exists
+		RepositoryDownload.class, // uses downloadUri
+		RepositoryInfo.class, // uses downloadUri
+		RepositoryLog.class, // uses downloadUri + info
+		RepositoryList.class, // uses downloadUri + info
+		// transactions
+		RepositoryTransaction.class, //
+		// single modifications
+		RepositoryMkdir.class, // uses transactions + (add)
+		RepositoryAdd.class, // uses transactions + mkdir
+		RepositoryDelete.class, // uses transactions + add + mkdir
+		RepositoryCopy.class, // uses transactions + add + mkdir
+		RepositoryMove.class, // uses transactions + add + mkdir + delete
+		RepositoryPropertiesSet.class, // uses transactions + add
+		RepositoryPropertiesDelete.class, // uses transactions + add + propertiesSet
+		RepositoryLocking.class, // uses add + copy + move
+// multiple modifications
+})
+public class SuiteIT {
+	// no code, just a placeholder class
+}
