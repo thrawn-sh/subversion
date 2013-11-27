de4a005e1519fe7f4e59f7fd3249b3046a49d526
Alexander Dreweke <alexander@dreweke.net>
2013-11-27 10:47:35 +0100

    wrapper to controll all apache instances

---
+++ setup/conf/opt/bin/apache_all.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+VERSIONS=""
+#VERSIONS="${VERSIONS} 1.0.0"
+#VERSIONS="${VERSIONS} 1.1.0"
+VERSIONS="${VERSIONS} 1.2.0"
+VERSIONS="${VERSIONS} 1.3.0"
+VERSIONS="${VERSIONS} 1.4.0"
+VERSIONS="${VERSIONS} 1.5.0"
+VERSIONS="${VERSIONS} 1.6.0"
+VERSIONS="${VERSIONS} 1.7.0"
+VERSIONS="${VERSIONS} 1.8.0"
+
+for version in ${VERSIONS}; do
+	service "apache-subversion_${version}" "${@}"
+done
