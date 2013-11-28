2e561eb0a173bb8b20fd1f1591210e5fcbef0d9c
Alexander Dreweke <alexander@dreweke.net>
2013-11-28 08:36:14 +0100

    update install script

--- setup/INSTALL.sh
+++ setup/INSTALL.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install necesarry components
-aptitude install apache2-mpm-worker rsync libxml2-utils
+aptitude install apache2 rsync ssl-cert libapache2-mod-proxy-html libxml2-utils
 
 # copy config
 rsync -acHv --no-p --no-o --no-g conf/* /
@@ -9,15 +9,7 @@ rsync -acHv --no-p --no-o --no-g conf/* /
 # prepare repository
 /sbin/create_svn_test
 
-# prepare ntlm
-service winbind restart
-adduser -p `openssl passwd -1 svnpass` svnuser
-smbpasswd -a svnuser <<EOF
-svnpass
-svnpass
-EOF
-adduser www-data winbindd_priv
-
-a2enmod auth_basic auth_digest auth_ntlm_winbind dav dav_fs dav_lock dav_svn ssl
+a2enmod  proxy_http ssl
 a2ensite default default-ssl
+
 service apache2 restart
