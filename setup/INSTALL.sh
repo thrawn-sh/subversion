117c86604517477e3aa31e7be8a518db3dff9334
Alexander Dreweke <alexander@dreweke.net>
2013-11-29 13:09:43 +0100

    start services after INSTALL

--- setup/INSTALL.sh
+++ setup/INSTALL.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install necesarry components
-aptitude install apache2 rsync ssl-cert libapache2-mod-proxy-html libxml2-utils
+aptitude install apache2 rsync ssl-cert zip libapache2-mod-proxy-html libxml2-utils
 
 # copy config
 rsync -acHv --no-p --no-o --no-g conf/* /
@@ -13,3 +13,13 @@ a2enmod  proxy_http ssl
 a2ensite default default-ssl
 
 service apache2 restart
+
+#chkconfig apache-subversion_1.0.0 on
+#chkconfig apache-subversion_1.1.0 on
+chkconfig apache-subversion_1.2.0 on
+chkconfig apache-subversion_1.3.0 on
+chkconfig apache-subversion_1.4.0 on
+chkconfig apache-subversion_1.5.0 on
+chkconfig apache-subversion_1.6.0 on
+chkconfig apache-subversion_1.7.0 on
+chkconfig apache-subversion_1.8.0 on
