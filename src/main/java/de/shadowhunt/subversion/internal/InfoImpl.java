8330c878598b9a528a6251f454098f2654b0246a
Alexander Dreweke <alexander@dreweke.net>
2014-11-01 15:26:52 +0100

    move Helper class to test

--- src/main/java/de/shadowhunt/subversion/internal/InfoImpl.java
+++ src/main/java/de/shadowhunt/subversion/internal/InfoImpl.java
@@ -15,8 +15,6 @@
  */
 package de.shadowhunt.subversion.internal;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Date;
