eb1cbcde9a7f418ab163d578c25dc1840c926753
Alexander Dreweke <alexander@dreweke.net>
2015-03-14 13:17:15 +0100

    indent

--- setup/conf/opt/subversion-frontend/conf/gen_locations.sh
+++ setup/conf/opt/subversion-frontend/conf/gen_locations.sh
@@ -2,21 +2,21 @@
 
 while read version port; do
     cat <<EOF
-<Location /${version}>
-    ProxyPass        http://localhost:${port}
-    ProxyPassReverse http://localhost:${port}
-    <if "(%{REQUEST_URI} != '/${version}/dump.zip') && (%{REQUEST_URI} != '/${version}/svn.zip')" >
-        Header           edit Location    localhost:${port} subversion.vm.shadowhunt.de/${version}
-        RequestHeader    edit Destination subversion.vm.shadowhunt.de/${version} localhost:${port}
-        SetInputFilter   Sed
-        InputSed         "s|href>/${version}/|href>/|g"
-        InputSed         "s|href><!\[CDATA\[/${version}/|href><!\[CDATA\[/|g"
-        InputSed         "s|src-path>/${version}/|src-path>/|g"
-        InputSed         "s|subversion.vm.shadowhunt.de/${version}|localhost:${port}|g"
-        SetOutputFilter  Sed
-        OutputSed        "s|href>/|href>/${version}/|g"
-    </if>
-</Location>
+    <Location /${version}>
+        ProxyPass        http://localhost:${port}
+        ProxyPassReverse http://localhost:${port}
+        <if "(%{REQUEST_URI} != '/${version}/dump.zip') && (%{REQUEST_URI} != '/${version}/svn.zip')" >
+            Header           edit Location    localhost:${port} subversion.vm.shadowhunt.de/${version}
+            RequestHeader    edit Destination subversion.vm.shadowhunt.de/${version} localhost:${port}
+            SetInputFilter   Sed
+            InputSed         "s|href>/${version}/|href>/|g"
+            InputSed         "s|href><!\[CDATA\[/${version}/|href><!\[CDATA\[/|g"
+            InputSed         "s|src-path>/${version}/|src-path>/|g"
+            InputSed         "s|subversion.vm.shadowhunt.de/${version}|localhost:${port}|g"
+            SetOutputFilter  Sed
+            OutputSed        "s|href>/|href>/${version}/|g"
+        </if>
+    </Location>
 EOF
 done <<EOF
 1.0.0 10000
@@ -30,29 +30,29 @@ EOF
 
 while read version port; do
     cat <<EOF
-<Location /${version}>
-    ProxyPass        http://localhost:${port}
-    ProxyPassReverse http://localhost:${port}
-    <if "(%{REQUEST_URI} != '/${version}/dump.zip') && (%{REQUEST_URI} != '/${version}/svn.zip')" >
-        Header           edit SVN-Me-Resource     /svn-basic /${version}/svn-basic
-        Header           edit SVN-Repository-Root /svn-basic /${version}/svn-basic
-        Header           edit SVN-Rev-Root-Stub   /svn-basic /${version}/svn-basic
-        Header           edit SVN-Rev-Stub        /svn-basic /${version}/svn-basic
-        Header           edit SVN-Txn-Root-Stub   /svn-basic /${version}/svn-basic
-        Header           edit SVN-Txn-Stub        /svn-basic /${version}/svn-basic
-        Header           edit SVN-VTxn-Root-Stub  /svn-basic /${version}/svn-basic
-        Header           edit SVN-VTxn-Stub       /svn-basic /${version}/svn-basic
-        Header           edit Location    localhost:${port} subversion.vm.shadowhunt.de/${version}
-        RequestHeader    edit Destination subversion.vm.shadowhunt.de/${version} localhost:${port}
-        SetInputFilter   Sed
-        InputSed         "s|href>/${version}/|href>/|g"
-        InputSed         "s|href><!\[CDATA\[/${version}/|href><!\[CDATA\[/|g"
-        InputSed         "s|src-path>/${version}/|src-path>/|g"
-        InputSed         "s|subversion.vm.shadowhunt.de/${version}|localhost:${port}|g"
-        SetOutputFilter  Sed
-        OutputSed        "s|href>/|href>/${version}/|g"
-    </if>
-</Location>
+    <Location /${version}>
+        ProxyPass        http://localhost:${port}
+        ProxyPassReverse http://localhost:${port}
+        <if "(%{REQUEST_URI} != '/${version}/dump.zip') && (%{REQUEST_URI} != '/${version}/svn.zip')" >
+            Header           edit SVN-Me-Resource     /svn-basic /${version}/svn-basic
+            Header           edit SVN-Repository-Root /svn-basic /${version}/svn-basic
+            Header           edit SVN-Rev-Root-Stub   /svn-basic /${version}/svn-basic
+            Header           edit SVN-Rev-Stub        /svn-basic /${version}/svn-basic
+            Header           edit SVN-Txn-Root-Stub   /svn-basic /${version}/svn-basic
+            Header           edit SVN-Txn-Stub        /svn-basic /${version}/svn-basic
+            Header           edit SVN-VTxn-Root-Stub  /svn-basic /${version}/svn-basic
+            Header           edit SVN-VTxn-Stub       /svn-basic /${version}/svn-basic
+            Header           edit Location    localhost:${port} subversion.vm.shadowhunt.de/${version}
+            RequestHeader    edit Destination subversion.vm.shadowhunt.de/${version} localhost:${port}
+            SetInputFilter   Sed
+            InputSed         "s|href>/${version}/|href>/|g"
+            InputSed         "s|href><!\[CDATA\[/${version}/|href><!\[CDATA\[/|g"
+            InputSed         "s|src-path>/${version}/|src-path>/|g"
+            InputSed         "s|subversion.vm.shadowhunt.de/${version}|localhost:${port}|g"
+            SetOutputFilter  Sed
+            OutputSed        "s|href>/|href>/${version}/|g"
+        </if>
+    </Location>
 EOF
 done <<EOF
 1.7.0 10700
