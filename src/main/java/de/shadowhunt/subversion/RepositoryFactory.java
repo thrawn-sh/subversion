81a44b3b6aa2664209e72cbcd7bad3b4ff5c1bb4
Alexander Dreweke <alexander@dreweke.net>
2015-11-11 15:35:26 +0100

    remove probe repository

--- src/main/java/de/shadowhunt/subversion/RepositoryFactory.java
+++ src/main/java/de/shadowhunt/subversion/RepositoryFactory.java
@@ -22,7 +22,6 @@ import java.util.ServiceLoader;
 import javax.annotation.concurrent.ThreadSafe;
 
 import org.apache.commons.lang3.Validate;
-import org.apache.http.HttpStatus;
 import org.apache.http.client.HttpClient;
 import org.apache.http.protocol.HttpContext;
 
@@ -52,22 +51,6 @@ public abstract class RepositoryFactory {
             return factory;
         }
         throw new SubversionException("Can not find a RepositoryFactory");
-    }
-
-    private static boolean isTolerableError(final int httpStatusCode) {
-        switch (httpStatusCode) {
-            case HttpStatus.SC_NOT_FOUND:
-                // part of the path does not exists
-                return true;
-            case HttpStatus.SC_FORBIDDEN:
-                // not (yet) the root of the repository (Repository.ProtocolVersion.HTTP_V1)
-                return true;
-            case HttpStatus.SC_BAD_REQUEST:
-                // not (yet) the root of the repository (Repository.ProtocolVersion.HTTP_V2)
-                return true;
-            default:
-                return false;
-        }
     }
 
     private static URI sanitise(final URI uri, final Resource path) {
@@ -105,47 +88,4 @@ public abstract class RepositoryFactory {
     }
 
     protected abstract Repository createRepository0(final URI saneUri, final HttpClient client, final HttpContext context, final boolean check);
-
-    /**
-     * Create a new {@link Repository} for given {@link URI} and use the given {@link HttpClient} with the {@link
-     * HttpClient} to connect to the server. <p>To find the {@link Repository} root the path is tested folder by folder
-     * till the root of the {@link Repository} root is found or no folders are left</p>
-     *
-     * @param repository {@link URI} to any resource of the repository (e.g: http://repository.example.net/svn/test_repo/folderA/subFolderB)
-     * @param client {@link HttpClient} that will handle all requests for this repository
-     * @param context {@link HttpContext} that will be used by all requests to this repository
-     *
-     * @return a new {@link Repository} for given {@link URI}
-     *
-     * @throws NullPointerException if any parameter is {@code null}
-     * @throws SubversionException if an error occurs during {@link Repository} probing and no {@link Repository} can be
-     * created
-     * @throws de.shadowhunt.subversion.TransmissionException if an error occurs in the underlining communication with
-     * the server
-     */
-    public Repository probeRepository(final URI repository, final HttpClient client, final HttpContext context) {
-        Validate.notNull(repository, "repository must not be null");
-        Validate.notNull(client, "client must not be null");
-        Validate.notNull(context, "context must not be null");
-
-        Resource path = Resource.create(repository.getPath());
-        while (true) {
-            try {
-                final URI saneUri = sanitise(repository, path);
-                return createRepository0(saneUri, client, context, true);
-            } catch (final SubversionException e) {
-                // ignore these errors while searching the correct repository root
-                if (!isTolerableError(e.getHttpStatusCode())) {
-                    throw e;
-                }
-            }
-
-            if (Resource.ROOT.equals(path)) {
-                break;
-            }
-            path = path.getParent();
-        }
-
-        throw new SubversionException("Could not find repository in path: " + repository.getPath());
-    }
 }
